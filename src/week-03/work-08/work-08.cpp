998dfd10da7bc54344968601c6a205c3133fe8fc
Nikita Pozdeev <octoberallover.nn@gmail.com>
2021-05-24 01:12:43 +0400

    added work-08 for third week

---
+++ src/week-03/work-08/work-08.cpp
@@ -0,0 +1,11 @@
+struct Incognizable {
+    int a, b;
+};
+
+int main() {
+    Incognizable a;
+    Incognizable b = {};
+    Incognizable c = { 0 };
+    Incognizable d = { 0, 1 };
+    return 0;
+}
