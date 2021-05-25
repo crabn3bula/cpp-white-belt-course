d6d3cfec3880ba67e3fcad29e4e1311fc45ae437
Nikita Pozdeev <octoberallover.nn@gmail.com>
2021-05-25 15:06:06 +0400

    added work-01 for fourth week

---
+++ src/week-04/work-01/work-01.cpp
@@ -0,0 +1,42 @@
+#include <string>
+
+struct Specialization {
+    std::string value;
+    explicit Specialization(const std::string& new_value) {
+        value = new_value;
+    }
+};
+
+struct Course {
+    std::string value;
+    explicit Course(const std::string& new_value) {
+        value = new_value;
+    }
+};
+
+struct Week {
+    std::string value;
+    explicit Week(const std::string& new_value) {
+        value = new_value;
+    }
+};
+
+struct LectureTitle {
+    std::string specialization;
+    std::string course;
+    std::string week;
+    LectureTitle(const Specialization& s, const Course& c, const Week& w) {
+        specialization = s.value;
+        course = c.value;
+        week = w.value;
+    }
+};
+
+int main() {
+    LectureTitle title(
+        Specialization("C++"),
+        Course("White belt"),
+        Week("4th")
+    );
+    return 0;
+}
